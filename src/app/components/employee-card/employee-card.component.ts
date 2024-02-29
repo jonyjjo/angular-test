e70df8357aea01d0fb672a5fdec7bee039eacbaa
marko mallari <mallari.markjomel@yahoo.com>
2024-02-29 16:34:42 +0000

    fix coding

--- src/app/components/employee-card/employee-card.component.ts
+++ src/app/components/employee-card/employee-card.component.ts
@@ -1,10 +1,36 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { IEmployee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'employee-card',
   templateUrl: './employee-card.component.html',
-  styleUrls: ['./employee-card.component.scss']
+  styleUrls: ['./employee-card.component.scss'],
 })
-export class EmployeeCardComponent {
+export class EmployeeCardComponent implements OnChanges {
+  @Input() employee!: IEmployee;
+  @Input() showTenured!: boolean;
+  @Input() showActive!: boolean;
+  @Output() newEventTrigger = new EventEmitter<string>();
 
+  constructor() {}
+
+  ngOnChanges() {
+    this.doSomething(this.showTenured, this.showActive);
+  }
+
+  private doSomething(tenured: boolean, active: boolean) {
+    setTimeout(() => {
+      let obj: any = {
+        tenured,
+        active,
+      };
+      this.newEventTrigger.emit(obj);
+    }, 0);
+  }
 }
--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -1,46 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { IEmployee } from 'src/app/models/employee.model';
+import { EmployeeService } from 'src/app/service/employee.service';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
 })
 export class EmployeesComponent implements OnInit {
   employees?: any[];
+  clonedEmployees?: any[];
   showTenured: boolean = false;
   showActive: boolean = false;
+  trackBy: any = 'id';
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) {}
 
   ngOnInit(): void {
     this.fetchEmployees();
   }
 
-
   fetchEmployees(): void {
-    // [Applicant] TODO: Fetch employees; fetch tenured employee IDs to set/map it in model
+    this.employeeService.getEmployees().subscribe((data: IEmployee[]) => {
+      this.employees = data;
+    });
 
-    //this.employeeService.getEmployees()
-    //this.employeeService.getTenuredEmployees()
-    this.employees = [
-      {
-        id: 1,
-        firstName: "mock",
-        lastName: "data",
-        position: "none"
-      }, {
-        id: 2,
-        firstName: "mock",
-        lastName: "datatwo",
-        position: "none"
-      },
-      {
-        id: 3,
-        firstName: "mock",
-        lastName: "datathree",
-        position: "none"
-      }
-    ];
+    this.employeeService.getTenuredEmployees().subscribe((data: any) => {
+      this.employees = this.employees?.map((v, i) => {
+        if (data?.ids.includes(v.id)) {
+          v.isTenured = true;
+        } else {
+          v.isTenured = false;
+        }
+        return v;
+      });
+      this.clonedEmployees = JSON.parse(JSON.stringify(this.employees));
+    });
   }
 
+  changeTracker(e: any) {
+    if (e.tenured && !e.active) {
+      this.clonedEmployees = this.employees?.filter(
+        (v) => v.isTenured === true
+      );
+    } else if (!e.tenured && e.active) {
+      this.clonedEmployees = this.employees?.filter((v) => v.isActive === true);
+    } else if (e.tenured && e.active) {
+      this.clonedEmployees = this.employees?.filter(
+        (v) => v.isTenured === true && v.isActive === true
+      );
+    } else {
+      this.clonedEmployees = this.employees;
+    }
+  }
 }
